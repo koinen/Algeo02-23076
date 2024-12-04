a267d7e123eee6de1994c98ddb7d1f5ef89e2757
pixelatedbus <luthfihakimyusra@gmail.com>
2024-12-04 16:28:19 +0700

    feat: pretty_midi, windowing and prepping audio file

---
+++ backend/modules/audio_processing.py
@@ -0,0 +1,60 @@
+import pretty_midi
+import numpy as np
+from general_processing import GeneralProcessing
+from typing import List
+
+# Note: start, end, pitch, velocity
+# process: get channel, normalize tempo and pitch, split channel into windowed beats (20 beats, 4 windows), make Tone Transition Vector and Tone Distribution Vector 
+# (Absolute Tone Based, Relative Tone Based, First Tone Based)
+
+class MidiProcessing:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    #TAKES FIRST CHANNEL ONLY, only takes start, end, pitch
+    def midiToMatrix(midi : pretty_midi.PrettyMIDI, bpm: int) -> np.ndarray:
+        matrixSong: List[List[float]] = []
+        #takes first channel
+        channel = midi.instruments[0]
+        for note in channel.notes:
+            #start, end, pitch
+            matrixSong.append([note.start, note.end, note.pitch, note.velocity])
+
+        res: np.ndarray = np.array(matrixSong)
+        return res
+        
+    @staticmethod
+    # window 20 beat, slide 4 beat
+    def windowingMatrix(matrix: np.ndarray, bpm: int) -> List[np.ndarray]:
+        time: float = 0
+        timeDelay: float = 60 / bpm
+        windowedMatrix: List[np.ndarray] = []
+        while time <= matrix[-1][1]:
+            currentWindow: np.ndarray = np.zeros((20, 4))
+            for i in range(20):
+                start_time = time + i * timeDelay
+                end_time = time + (i + 1) * timeDelay
+                notes_in_window = matrix[(start_time <= matrix[:, 0]) & (matrix[:, 0] < end_time)]
+                
+                # ambil velocity paling besar
+                if len(notes_in_window) > 1:
+                    max_velocity = notes_in_window[0][3]
+                    max_note = notes_in_window[0]
+                    for note in notes_in_window:
+                        if note[3] > max_velocity:
+                            max_velocity = note[3]
+                            max_note = note
+                    currentWindow[i] = max_note
+                elif len(notes_in_window) > 0:
+                    currentWindow[i] = notes_in_window[0]                    
+            windowedMatrix.append(currentWindow)
+            time += 4 * timeDelay
+        return windowedMatrix
+
+        
+    
+midi = pretty_midi.PrettyMIDI("./backend/modules/MIDI_sample.mid")
+matrix = MidiProcessing.midiToMatrix(midi, 120)
+windowed = MidiProcessing.windowingMatrix(matrix, 120)
+print(windowed)
