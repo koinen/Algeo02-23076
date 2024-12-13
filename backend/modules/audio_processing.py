401aa61f30ecfd2273a35cdc8cdf389756a85cb7
pixelatedbus <luthfihakimyusra@gmail.com>
2024-12-14 03:37:28 +0700

    Update audio_processing.py

--- backend/modules/audio_processing.py
+++ backend/modules/audio_processing.py
@@ -1,11 +1,8 @@
 import pretty_midi
 import numpy as np
-<<<<<<< HEAD
 from .atb import histograms
 from .general_processing import GeneralProcessing
 from typing import List
-=======
->>>>>>> audio
 
 # Note: start, end, pitch, velocity
 # process: get channel, normalize tempo and pitch, split channel into windowed beats (20 beats, 4 windows), make Tone Transition Vector and Tone Distribution Vector 
@@ -15,10 +12,15 @@ class MidiProcessing:
     def __init__(self):
         pass
 
-<<<<<<< HEAD
+    def create_fuzzy_histogram(data, bins, sigma):
+        histogram = np.zeros(len(bins))
+        
+        for value in data:
+            weights = np.exp(-((bins - value) ** 2) / (2 * sigma ** 2))
+            histogram += weights
+
+        histogram /= np.sum(histogram)
     @staticmethod
-=======
->>>>>>> audio
     def create_fuzzy_histogram(data, bins, sigma):
         histogram = np.zeros(len(bins))
         
@@ -30,15 +32,13 @@ class MidiProcessing:
         
         return histogram
 
+        return histogram
+
     @staticmethod
-<<<<<<< HEAD
     def processMidi(path):
         WINDOW_SIZE = 20 
         HOP_SIZE = 4
         midi_data = pretty_midi.PrettyMIDI(path)
-=======
-    def makeMidi(midi_data, song_idx, window_size, hop_size):
->>>>>>> audio
         melody_notes = midi_data.instruments[0].notes
         queryBPM = midi_data.estimate_tempo()
         melody_notes = np.array([[note.start, note.end, note.pitch, note.velocity] for note in melody_notes])
@@ -46,7 +46,6 @@ class MidiProcessing:
             note[0] = note[0] * (120 / queryBPM) #BPM = 120
             note[1] = note[1] * (120 / queryBPM)
         
-<<<<<<< HEAD
         beat = 60 / 120 #BPM
         windows = []
         current_time = 0
@@ -56,19 +55,6 @@ class MidiProcessing:
 
         windows = [window for window in windows if len(window) > 0]
         
-=======
-        song_idx = [song_idx] * len(melody_notes)
-        
-        beat = 60 / 120 #BPM
-        windows = []
-        current_time = 0
-        while current_time + window_size * beat < melody_notes[-1][1]:
-            windows.append(melody_notes[(melody_notes[:, 0] >= current_time) & (melody_notes[:, 0] < current_time + window_size * beat)][:, 2])
-            current_time += hop_size * beat
-
-        windows = [window for window in windows if len(window) > 0]
-        
->>>>>>> audio
         window_feature_vector = np.array([])
         for window in windows:
             first_tone = window[0]
@@ -78,21 +64,5 @@ class MidiProcessing:
             current_window_feature_vector = np.concatenate((fuzzy_atb, fuzzy_rtb, fuzzy_ftb))
             
             window_feature_vector = np.append(window_feature_vector, current_window_feature_vector)
-<<<<<<< HEAD
         
         return window_feature_vector
-=======
-
-        return window_feature_vector, song_idx
-
-
-if __name__ == "__main__":
-    midi = pretty_midi.PrettyMIDI("../uploads/MIDI_sample.mid")
-    matrix = MidiProcessing.makeMidi(midi, 0, 30, 6)
-    for i in matrix[0]:
-        print(i)
-    # windowedBeats = MidiProcessing.windowedBeats(matrix, 20, 4)
-    # print(windowedBeats)
-    # print(matrix)
-    # print(windowedBeats)
->>>>>>> audio
