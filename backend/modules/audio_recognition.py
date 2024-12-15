6fd4deff14fc9ebdec86014d03f3fbd722426bef
pixelatedbus <luthfihakimyusra@gmail.com>
2024-12-15 15:12:01 +0700

    feat: wav_to_midi

--- backend/modules/audio_recognition.py
+++ backend/modules/audio_recognition.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import pretty_midi
 
-
+## DEPRECATED
 def openProcessedDatabaseMIDI():
     if not os.path.exists("../uploads/processed/project_dataset_midi.npy"):
         return None
@@ -15,7 +15,7 @@ def processQueryMIDI(midi_file_name):
     window_feature_vectors = MidiProcessing.processMidi(f"../uploads/{midi_file_name}")
     return window_feature_vectors
 
-def queryMIDI(midi_file_name):
+def queryMIDI(midi_file_name, min_similarity=0.83):
     project_query = processQueryMIDI(midi_file_name)
     # project_dataset = np.load("../uploads/processed/project_dataset_midi.npy") ### dataset[i][j][k] = i-th song, j-th window, k-th feature
     project_dataset = np.load("../uploads/dataset/audio_data.npy", allow_pickle=True)
@@ -26,7 +26,14 @@ def queryMIDI(midi_file_name):
                 distance = GeneralProcessing.cosineSimilarity(project_query[i], project_dataset[j][k])
                 closest.append((distance, i, j, k))
     #check absolute window for now, might change to check song                
-    top_5 = sorted(closest, key=lambda x: x[0], reverse=True)[:5]
-    return top_5
+    top = sorted(closest, key=lambda x: x[0], reverse=True) 
+    top = [top[i] for i in range(len(top)) if top[i][0] >= min_similarity and top[i][2] not in [top[j][2] for j in range(i)]]
+    
+    array = []
+    with open("../uploads/dataset/audio_file_names.txt", 'r') as file:
+        array = [line.strip() for line in file]
+
+    top = [array[top[i][2]] for i in range(len(top))]
+    return top
 
     
